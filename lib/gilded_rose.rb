83134787b5e8294f1306f501ea52c841910bea93
ianmcnicholas <ianjmcnicholas@.com>
2021-04-08 16:19:31 +0100

    update_quality() method refactored to use cases instead of nested if statements

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -6,52 +6,29 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality -= 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality += 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality += 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
+      case item.name
+      when "Sulfuras, Hand of Ragnaros"
+        return item
+      when "Aged Brie"
         item.sell_in -= 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality -= 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality += 1
-          end
-        end
+        item.quality += 2 if item.sell_in < 0 && item.quality <= 48
+        item.quality += 1 if item.sell_in < 0 && item.quality < 50
+        item.quality += 1 if item.sell_in >= 0 && item.quality < 50
+      when "Backstage passes to a TAFKAL80ETC concert"
+        item.sell_in -= 1
+        item.quality += 1 if item.sell_in > 10 && item.quality < 50
+        item.quality += 2 if item.sell_in <= 10 && item.sell_in > 5 && item.quality <= 48
+        item.quality += 3 if item.sell_in <= 5 && item.sell_in >= 0 && item.quality <= 47
+        item.quality = 0 if item.sell_in < 0
+      else
+        item.sell_in -= 1
+        item.quality -= 1 if item.sell_in >= 0 && item.quality > 0
+        item.quality -= 2 if item.sell_in < 0 && item.quality > 0
       end
     end
   end
 end
+
 
 class Item
   attr_accessor :name, :sell_in, :quality
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -36,6 +36,14 @@ describe GildedRose do
       expect(items[0].quality).to eq 11
     end
 
+    it "Aged Brie increases 2 in Quality with age after sell by date" do
+      items = [Item.new("Aged Brie", 0, 48)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].name).to eq "Aged Brie"
+      expect(items[0].sell_in).to eq -1
+      expect(items[0].quality).to eq 50
+    end
+
     it "the Quality of an item is never more than 50" do
       items = [Item.new("Aged Brie", 5, 50)]
       GildedRose.new(items).update_quality()
