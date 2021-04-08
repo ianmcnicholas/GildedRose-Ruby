3266735ac9a6a81646bf67155bcdc76c9b9032ce
ianmcnicholas <ianjmcnicholas@.com>
2021-04-08 16:22:52 +0100

    update_quality() method extracted into private methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -6,29 +6,43 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
+      @item = item
       case item.name
       when "Sulfuras, Hand of Ragnaros"
         return item
       when "Aged Brie"
-        item.sell_in -= 1
-        item.quality += 2 if item.sell_in < 0 && item.quality <= 48
-        item.quality += 1 if item.sell_in < 0 && item.quality < 50
-        item.quality += 1 if item.sell_in >= 0 && item.quality < 50
+        aged_brie_update
       when "Backstage passes to a TAFKAL80ETC concert"
-        item.sell_in -= 1
-        item.quality += 1 if item.sell_in > 10 && item.quality < 50
-        item.quality += 2 if item.sell_in <= 10 && item.sell_in > 5 && item.quality <= 48
-        item.quality += 3 if item.sell_in <= 5 && item.sell_in >= 0 && item.quality <= 47
-        item.quality = 0 if item.sell_in < 0
+        backstage_pass_update
       else
-        item.sell_in -= 1
-        item.quality -= 1 if item.sell_in >= 0 && item.quality > 0
-        item.quality -= 2 if item.sell_in < 0 && item.quality > 0
+        normal_update
       end
     end
   end
 end
 
+  private
+
+  def aged_brie_update
+    @item.sell_in -= 1
+    @item.quality += 2 if @item.sell_in < 0 && @item.quality <= 48
+    @item.quality += 1 if @item.sell_in < 0 && @item.quality < 50
+    @item.quality += 1 if @item.sell_in >= 0 && @item.quality < 50
+  end
+
+  def backstage_pass_update
+    @item.sell_in -= 1
+    @item.quality += 1 if @item.sell_in > 10 && @item.quality < 50
+    @item.quality += 2 if @item.sell_in <= 10 && @item.sell_in > 5 && @item.quality <= 48
+    @item.quality += 3 if @item.sell_in <= 5 && @item.sell_in >= 0 && @item.quality <= 47
+    @item.quality = 0 if @item.sell_in < 0
+  end
+
+  def normal_update
+    @item.sell_in -= 1
+    @item.quality -= 1 if @item.sell_in >= 0 && @item.quality > 0
+    @item.quality -= 2 if @item.sell_in < 0 && @item.quality > 0
+  end
 
 class Item
   attr_accessor :name, :sell_in, :quality
